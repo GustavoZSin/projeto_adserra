acd6fddc36050d6fb7c3731d2db179646d8dc97c
Gustavo Zampieri <110244543+GustavoZSin@users.noreply.github.com>
2026-04-21 22:16:02 -0300

    Implementa autenticação e controllers básicos na API
    Adiciona AuthController e HomeController para autenticação e rota protegida. Cria DTO LoginDto e classe User integrando Identity. Implementa ProfessorService para manipulação de professores. Configura Identity, autenticação, cookies, dependências e Swagger no Program.cs. Adiciona rotas de login, logout e consulta de usuário autenticado.

---
+++ API/Services/ProfessorService.cs
@@ -0,0 +1,39 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services
+{
+    public class ProfessorService
+    {
+        private readonly AppDbContext _context;
+        public ProfessorService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        internal async Task<bool> CriarProfessorAsync(string nomeCompleto, string matricula, string emailInstitucional, string departamento, string idUserIdentity, User userIdentity)
+        {
+            var professor = new Professor
+            {
+                NomeCompleto = nomeCompleto,
+                Matricula = matricula,
+                EmailInstitucional = emailInstitucional,
+                Departamento = departamento,
+                IdUsuario = idUserIdentity,
+                Usuario = userIdentity
+            };
+
+            _context.Professores.Add(professor);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        internal async Task<Professor> ObterPorMatricula(string matricula)
+        {
+            return await _context.Professores.FirstOrDefaultAsync(p => p.Matricula == matricula);
+        }
+    }
+}
