ea0ce36b5c8f6b38956720ee3054af6297e0f620
Gustavo Zampieri <110244543+GustavoZSin@users.noreply.github.com>
2026-04-26 16:51:41 -0300

    Implementa cadastro e gestão de usuários admin
    Adiciona endpoint para registrar admin, seed de roles e usuário admin inicial. Cria controller e serviço para listar e deletar usuários, além dos DTOs necessários para cadastro e resposta.

---
+++ API/Services/UsuariosService.cs
@@ -0,0 +1,51 @@
+﻿using API.Data;
+using API.DTOs.Response;
+using API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services
+{
+    public class UsuariosService
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public UsuariosService(AppDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        internal async Task<List<ListarUsuarioResponse>> ListarUsuariosAsync()
+        {
+            var response = _context.Professores
+            .Select(p => new ListarUsuarioResponse
+            {
+                Id = p.Id,
+                NomeCompleto = p.NomeCompleto,
+                Matricula = p.Matricula,
+                EmailInstitucional = p.EmailInstitucional,
+                Departamento = p.Departamento
+            });
+
+            return await response.ToListAsync();
+        }
+
+        public async Task<bool> DeletarUsuarioAsync(int id)
+        {
+            var professor = await _context.Professores
+                .Include(p => p.Usuario)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (professor == null)
+                return false;
+
+            _context.Professores.Remove(professor);
+            await _userManager.DeleteAsync(professor.Usuario);
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
