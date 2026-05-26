705ec73a411bbfb065f8d8eb6b157e0119aab8ba
Gustavo Zampieri <110244543+GustavoZSin@users.noreply.github.com>
2026-05-26 16:27:13 -0300

    Migração de dados

---
+++ API/Migrations/20260526192443_AddIndexParaPerformance.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexParaPerformance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Publicacoes_Data",
+                table: "Publicacoes",
+                column: "Data");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Publicacoes_Tipo_Publica",
+                table: "Publicacoes",
+                columns: new[] { "Tipo", "Publica" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notificacoes_IdUsuario_Lida",
+                table: "Notificacoes",
+                columns: new[] { "IdUsuario", "Lida" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Publicacoes_Data",
+                table: "Publicacoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Publicacoes_Tipo_Publica",
+                table: "Publicacoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notificacoes_IdUsuario_Lida",
+                table: "Notificacoes");
+        }
+    }
+}
